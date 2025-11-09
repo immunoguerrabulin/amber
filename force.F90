0abfffea5edb9b98b50257d92d20e172db9718fb
immunoguerrabulin <106614547+immunoguerrabulin@users.noreply.github.com>
2025-11-09 12:04:15 -0600

    Add files via upload

--- force.F90
+++ force.F90
@@ -36,9 +36,7 @@
 subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
                  onereff, qsetup, do_list_update, nstep)
 
-  ! FIRES controls and local-slice helper
-  use qmmm_fires_module, only: fires, fires_force, fires_set_local_bounds, &
-       fires_in_force, fire_inner, fire_outer, mts_fires, mts_n
+  use qmmm_fires_module, only: fires,fires_force,fire_inner,fire_outer, mts_fires, mts_n  !JOSE MOD: include MTS controls
   
 #if !defined(DISABLE_NFE)
   use nfe_sander_hooks, only: nfe_on_force => on_force
@@ -210,7 +208,6 @@ subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
 #include "multitmd.h"
 #include "flocntrl.h"
   integer istart, iend
-  integer :: istart3, iend3     ! local 3N bounds for FIRES
   _REAL_ evdwex, eelex
   _REAL_ eextpot
   _REAL_ enemap
@@ -222,7 +219,6 @@ subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
   _REAL_  enmr(6), devdis(4), devang(4), devtor(4), devpln(4), devplpt(4), &
        devgendis(4), entr, ecap, enfe
   _REAL_ efires
-  _REAL_ efires_local          ! local FIRES energy before MPI reduce
   _REAL_  x(*), f(*), vir(4)
   type(state_rec)  ener
 
@@ -310,7 +306,6 @@ subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
   end if
   ene(:) = ZERO
   call zero_pot_energy(pot)
-  fires_in_force = .false.
 
   belly = ibelly > 0
 
@@ -354,11 +349,6 @@ subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
   istart = 1
   iend = natom
 #endif /* MPI */
-
-  ! Set FIRES local 3N bounds for this rank (safe each outer step)
-  istart3 = 3*istart - 2
-  iend3   = 3*iend
-  call fires_set_local_bounds(istart3, iend3)
 
   ! QM/MM variable QM solvent scheme
   if (qmmm_nml%ifqnt .and. qmmm_nml%vsolv > 0) then
@@ -1228,25 +1218,11 @@ subroutine force(xx, ix, ih, ipairs, x, f, ener, vir, fs, rborn, reff, &
   end if
 #endif
 
-  efires       = 0.0d0
-  efires_local = 0.0d0
-  ! Non-MTS FIRES path: apply FIRES once per outer step here
-  fires_in_force = .false.
-  if (fires == 1 .and. ipimd == 0) then
-    if (.not. (mts_n > 1 .and. mts_fires > 0.0d0)) then
-      ! Recompute local bounds in case of rebalancing and set them
-      istart3 = 3*istart - 2
-      iend3   = 3*iend
-      call fires_set_local_bounds(istart3, iend3)
-      call fires_force(x, xx(lmass), natom, f, efires_local)
-#ifdef MPI
-      call MPI_Allreduce(efires_local, efires, 1, MPI_DOUBLE_PRECISION, MPI_SUM, commsander, ierr)
-#else
-      efires = efires_local
-#endif
-      pot%constraint = pot%constraint + efires
-      fires_in_force = .true.
-    end if
+
+
+  if (fires==1) then
+    !JOSE MOD: FIRES is the fast force — evaluate every step (no MTS caching)
+    call fires_force(x,xx(lmass),natom,f,efires)
   end if
  
   
